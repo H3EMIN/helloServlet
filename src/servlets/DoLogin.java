f3a62793007fe7b0f0aa5739c4c31e0e01d901ec
h22min <ccimi@Joshua>
2016-09-20 13:58:31 +0900

    setup my servlet project

---
+++ src/servlets/DoLogin.java
@@ -0,0 +1,45 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DoLogin
+ */
+@WebServlet("/DoLogin")
+public class DoLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DoLogin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//read form data
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		//perform business login
+		PrintWriter out = response.getWriter();
+		//build HTML code
+		String htmlResponse ="<html>";
+		htmlResponse += "<h2> Your name is :" + username + "<br/>";
+		htmlResponse += "<h2> Your password is :" + password + "<br/>";
+		htmlResponse += "</html>";
+		out.println(htmlResponse);
+		
+	}
+
+}
